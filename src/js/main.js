36bc122ba9449147df40ba1fe4fc347a8e868e24
VictorMinsk <86519204+VictorMinsk@users.noreply.github.com>
2024-03-06 21:51:52 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("span.registr-link").click(function () {
+    $(".no-show").removeClass("no-show");
+    $(".show").addClass("no-show");
+  });
+  $(".activated").click(function () {
+    $(".no-show").removeClass("no-show");
+    $(".show").addClass("no-show");
+  });
+});
